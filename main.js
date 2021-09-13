45268f85c20b7c74029ddfd14d6c8a45da6bc73d
Maurice Markslag <maurice.markslag1996@gmail.com>
2021-09-13 17:23:18 +0200

    working on header section

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-const dropdownToggler = document.getElementById('')
+const modal = document.querySelector('.modal-content');
+
+const button = document.getElementById('btn');
+
+
+button.onclick = () => {
+    modal.style.display = "flex";
+};
+
+span.onclick = () => {
+    modal.style.display = "none";
+};
+
+window.onclick = event => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
