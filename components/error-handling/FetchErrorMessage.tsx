9e878082c877e193ed5fa3d3beb17b27c4ecbca9
Maurice Markslag <maurice.markslag1996@gmail.com>
2021-10-06 14:39:31 +0200

    set error messages for no entries and network errors

---
+++ components/error-handling/FetchErrorMessage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { errorMessageInterface } from "../../pages";
+import * as Styled from "./FetchErrorMessage.styled";
+
+interface FetchErrorMessageProps {
+	message: errorMessageInterface
+}
+
+function FetchErrorMessage({ message }: FetchErrorMessageProps) {
+  return (
+    <Styled.ErrorMessageWrapper backgroundColor={message.color} border={message.border}>
+      <Styled.FetchErrorMessage>{message.error}</Styled.FetchErrorMessage>
+      <Styled.FetchErrorSubMessage>{message.submessage}</Styled.FetchErrorSubMessage>
+    </Styled.ErrorMessageWrapper>
+  );
+}
+
+export default FetchErrorMessage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { ThemeProvider } from "styled-components";
 
-import { getTimeEntries } from "../services/getTimeEntries";
+import { getTimeEntries, NotFoundError } from "../services/getTimeEntries";
 import { Icon } from "../components/icon/Icon";
 import { theme } from "../styles/theme";
 import { TimeEntryInterface } from "../components/interface";
@@ -12,20 +12,46 @@ import GlobalStyle from "../styles/global";
 import Header from "../components/header/Header";
 import Subheader from "../components/subheader/subheader";
 import TimeEntries from "../components/time-entries/TimeEntries";
+import FetchErrorMessage from "../components/error-handling/FetchErrorMessage";
+
+export interface errorMessageInterface {
+  error: string,
+  submessage: string,
+  color: string,
+  border: string
+}
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [timeEntries, setTimeEntries] = useState([]);
+  const [errorMessage, setErrorMessage] = useState<errorMessageInterface>();
 
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
 
-  console.log(timeEntries);
-
   useEffect(() => {
     async function fetchTimeEntries() {
-      setTimeEntries(await getTimeEntries());
+      const response = await getTimeEntries();
+
+      if (response instanceof NotFoundError) {
+        setErrorMessage({
+          border: "red",
+          color: "#ffbab9",
+          error: "Oops... Something went wrong while loading your data 😭",
+          submessage: "Please try again later, or contact your developer at developer@humanoids.nl",
+        });
+        return;
+      }
+      if (response.length === 0) {
+        setErrorMessage({
+          border: "yellow",
+          color: "#feffac",
+          error: "No entries have been found yet",
+          submessage: "Enter your first Time Entry 😎",
+        });
+      }
+      setTimeEntries(response);
     }
     fetchTimeEntries();
   }, []);
@@ -57,6 +83,7 @@ function App() {
           )}
           <EntryForm isOpen={isOpen} onClose={handleClick} onSubmit={addNewTimeEntry} />
           <TimeEntries timeEntries={timeEntries} />
+          {timeEntries.length === 0 && <FetchErrorMessage message={errorMessage} />}
         </Styled.FirstPageWrapper>
       </ThemeProvider>
     </>
