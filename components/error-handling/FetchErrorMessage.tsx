73779807cdd3886b4156c6a4eda6aabf59de182d
Maurice Markslag <maurice.markslag1996@gmail.com>
2021-10-06 14:49:12 +0200

    fix minor bug

--- components/error-handling/FetchErrorMessage.tsx
+++ components/error-handling/FetchErrorMessage.tsx
@@ -3,14 +3,14 @@ import { errorMessageInterface } from "../../pages";
 import * as Styled from "./FetchErrorMessage.styled";
 
 interface FetchErrorMessageProps {
-	message: errorMessageInterface
+  message: errorMessageInterface;
 }
 
 function FetchErrorMessage({ message }: FetchErrorMessageProps) {
   return (
-    <Styled.ErrorMessageWrapper backgroundColor={message.color} border={message.border}>
-      <Styled.FetchErrorMessage>{message.error}</Styled.FetchErrorMessage>
-      <Styled.FetchErrorSubMessage>{message.submessage}</Styled.FetchErrorSubMessage>
+    <Styled.ErrorMessageWrapper backgroundColor={message?.color} border={message?.border}>
+      <Styled.FetchErrorMessage>{message?.error}</Styled.FetchErrorMessage>
+      <Styled.FetchErrorSubMessage>{message?.submessage}</Styled.FetchErrorSubMessage>
     </Styled.ErrorMessageWrapper>
   );
 }
