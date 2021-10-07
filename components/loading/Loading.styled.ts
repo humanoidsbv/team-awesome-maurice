bfffa32d2cccf4b99e9dc95b1d52bba3afc900a1
Maurice Markslag <maurice.markslag1996@gmail.com>
2021-10-07 11:22:21 +0200

    minor changes based on review 2/2

--- components/loading/Loading.styled.ts
+++ components/loading/Loading.styled.ts
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 export const Loading = styled.h1`
-	color: grey;
-	margin-top: 100px;
-	opacity: 0.7;
-	text-align: center;
+  color: #808080;
   font-family: ${({ theme }) => theme.fontSecondary};
+  margin-top: 100px;
+  opacity: 0.7;
+  text-align: center;
 `;
