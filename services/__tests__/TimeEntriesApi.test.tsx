a3b188f6b1e2a4974fe0b0c2eef7642382db45df
Maurice Markslag <maurice.markslag1996@gmail.com>
2021-10-21 11:58:51 +0200

    move mockedTimeEntries to separate file

--- services/__tests__/TimeEntriesApi.test.tsx
+++ services/__tests__/TimeEntriesApi.test.tsx
@@ -1,28 +1,5 @@
 import { getTimeEntries } from "../time-entries-api";
-
-const mockedTimeEntries = [
-  {
-    activity: "testing",
-    client: "Humanoids",
-    endTime: "2020-03-10T16:30:00.000Z",
-    id: 1,
-    startTime: "2020-03-10T09:30:00.000Z",
-  },
-  {
-    activity: "testing",
-    client: "Humanoids",
-    endTime: "2020-03-10T16:30:00.000Z",
-    id: 2,
-    startTime: "2020-03-10T09:30:00.000Z",
-  },
-  {
-    activity: "testing",
-    client: "Humanoids",
-    endTime: "2020-03-10T16:30:00.000Z",
-    id: 2,
-    startTime: "2020-03-10T09:30:00.000Z",
-  },
-];
+import { mockedTimeEntries } from "../../fixtures/mockedTimeEntries";
 
 it("fetches TimeEntries from the server", () => {
   const mockJsonPromise = Promise.resolve(mockedTimeEntries);
